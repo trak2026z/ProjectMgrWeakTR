382129ad6925bd9797e138ff7c40106befaeb052
Maciej Pietrasz <m.pietrasz@devcodi.pl>
2025-08-04 17:31:52 +0200

    fix email

--- CarWorkshop.MVC/CarWorkshop.Application/CarWorkshop/CarWorkshopDto.cs
+++ CarWorkshop.MVC/CarWorkshop.Application/CarWorkshop/CarWorkshopDto.cs
@@ -19,7 +19,6 @@ namespace CarWorkshop.Application.CarWorkshop
 
         [StringLength(12, MinimumLength = 8)]
         public string PhoneNumber { get; set; } = default!;
-        public string Email { get; set; } = default!;
         public string Street { get; set; } = default!;
         public string City { get; set; } = default!;
         public string PostalCode { get; set; } = default!;
